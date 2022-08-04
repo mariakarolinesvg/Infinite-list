c6b3d179fab3657a37b0cc5d7dfd4a40c9e22efa
mariapalito <mklp1@aluno.ifal.edu.br>
2022-08-04 17:31:04 -0300

    Adição do arquivo baby3

---
+++ Lista Baby/baby3.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia um número e o imprima caso ele seja menor que 200
+
+numero = float(input("Informe um número: "))
+
+if numero < 200:
+    print(numero)
+else: 
+    print("Número maior ou igual a 200!")
