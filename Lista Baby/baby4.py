0e4e323ed50666365e31b2f4ff85a52f03385ede
mariapalito <mklp1@aluno.ifal.edu.br>
2022-08-04 17:35:46 -0300

    Adição do arquivo baby4

---
+++ Lista Baby/baby4.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que leia um número e o imprima caso ele seja maior que 190
+
+numero = float(input("Informe um número: "))
+
+if numero > 190:
+    print(numero)
+else: 
+    print("Número menor ou igual a 190!")
